2ad20a0bb0f5b35646a6e071bdc360c54ff797b9
skinwalker <skinwalker>
2016-12-04 19:51:37 +0200

    The first part of the blog.

---
+++ SoftuniBlog/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using SoftuniBlog.Models;
+using SoftuniBlog.Migrations;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(SoftuniBlog.Startup))]
+namespace SoftuniBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlogDbContext, Configuration>());
+            ConfigureAuth(app);
+        }
+    }
+}
